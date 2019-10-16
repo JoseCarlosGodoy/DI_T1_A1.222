be6c3144d29bcd5c673bc1cf342a7ca0b3d1fb8a
Jose Carlos PC <Jose Carlos PC@DESKTOP-9NQ8R39>
2019-10-16 18:42:30 +0200

    Actividad 1 Ejercicio 2.2

---
+++ src/colorfulcircles/ColorfulCircles.java
@@ -0,0 +1,96 @@
+package colorfulcircles;
+
+
+import static java.lang.Math.random;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class ColorfulCircles extends Application {
+ 
+    @Override
+    
+    public void start(Stage primaryStage) {
+        
+        //Crea la ventana principal
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        primaryStage.setScene(scene);
+        
+        //Grafica   
+        Group circles = new Group();
+            for (int i = 0; i < 30; i++) {
+                 Circle circle = new Circle(150, Color.web("white", 0.05));
+                circle.setStrokeType(StrokeType.OUTSIDE);
+                circle.setStroke(Color.web("white", 0.16));
+                circle.setStrokeWidth(4);
+                circles.getChildren().add(circle);
+            }
+            
+        //Crea el gradiente multicolor de fondo    
+        Rectangle colors = new Rectangle(scene.getWidth(), scene.getHeight(),
+        new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new 
+         Stop[]{
+            new Stop(0, Color.web("#f8bd55")),
+            new Stop(0.14, Color.web("#c0fe56")),
+            new Stop(0.28, Color.web("#5dfbc1")),
+            new Stop(0.43, Color.web("#64c2f8")),
+            new Stop(0.57, Color.web("#be4af7")),
+            new Stop(0.71, Color.web("#ed5fc2")),
+            new Stop(0.85, Color.web("#ef504c")),
+            new Stop(1, Color.web("#f2660f")),}));
+        colors.widthProperty().bind(scene.widthProperty());
+        colors.heightProperty().bind(scene.heightProperty());
+        
+        //Introduce el fondo negro y le da las propiedades multicolor a los circulos
+        Group blendModeGroup = 
+        new Group(new Group(new Rectangle(scene.getWidth(), scene.getHeight(),
+        Color.BLACK), circles), colors);
+        colors.setBlendMode(BlendMode.OVERLAY);
+        root.getChildren().add(blendModeGroup);
+        //Caracteristicas de los circulos
+        circles.setEffect(new BoxBlur(10, 10, 3));
+        
+        //Añade la animacion de los circulos
+        Timeline timeline = new Timeline();
+            for (Node circle: circles.getChildren()) {
+                timeline.getKeyFrames().addAll(
+                    //Direcciona aleatoriamente los circulos en el momento 0
+                    new KeyFrame(Duration.ZERO, // set start position at 0
+                    new KeyValue(circle.translateXProperty(), random() * 800),
+                    new KeyValue(circle.translateYProperty(), random() * 600)
+                    ),
+                    new KeyFrame(new Duration(40000), // set end position at 40s
+                    //Direcciona aleatoriamente los circulos en la ventana durante la ejecucion
+                    new KeyValue(circle.translateXProperty(), random() * 800),
+                    new KeyValue(circle.translateYProperty(), random() * 600)
+                    )
+                );
+            }
+        // play 40s of animation
+        timeline.play();
+        //Muestra los resultados
+        primaryStage.show();
+        
+     
+    }
+
+ public static void main(String[] args) {
+        launch(args);
+  }
+}
